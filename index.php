7972802a50ef0f9ad44c8d4a6f2d03d6713216ab
Alefer Reinert <aleferreinert@gmail.com>
2021-06-11 19:11:08 -0300

    Criado lista de produtos usando grid layout.

--- index.php
+++ index.php
@@ -12,6 +12,24 @@
 			<li><a href="#"><img src="assets/img/content/banner-2.jpg" alt="Roupas infantis"></a></li>
 			<li><a href="#"><img src="assets/img/content/banner-3.jpg" alt="Lançamentos"></a></li>
 		</ul>
+
+		<section class="products">
+			<div class="container">
+				<h1 class="title-section">Promoções</h1>
+				<ul class="list-products">
+					<?php for($i = 1; $i<= 12; $i++) :  ?>
+					<li>
+						<a href="" class="product">
+							<img class="image" src="assets/img/content/product-1.jpg" alt="">
+							<h2 class="name">Camiseta Adidas Trefoil</h2>
+							<div class="price-original">De <span>R$ 99,90</span></div>
+							<div class="price-final">Por <span>R$ 79,90</span></div>
+						</a>
+					</li>
+					<?php endfor; ?>
+				</ul>
+			</div>
+		</section>
 	</main>
 	
 	<?php include 'inc/footer.php'; ?>
